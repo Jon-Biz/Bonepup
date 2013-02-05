a0aa9ad68e241cd3f19ea6ac0de3f1f21685a587
Jonathan El-Bizri <git-commit@elbizri.com>
2013-02-04 16:15:36 -0800

    update custom-fields request, insert convenience access to first product model

--- theme/app/modules/data-create.js
+++ theme/app/modules/data-create.js
@@ -10,7 +10,7 @@ App.module("Data", function(Data){
 		
 		// Add all custom fields required to this comma delimited list, or they won't be pulled
 		
-		Data.Pages.url = "/?json=get_page_index&custom_fields=Brand,Client,closeup,description,product_image,Product,Varietals,zoomx1,zoomx2";
+		Data.Pages.url = "/?json=get_page_index&custom_fields=Brand,Client,closeup,description,product_image,Product,Varietals,zoomx1,zoomy1,zoomx2,zoomy2";
 		Data.Pages.fetch({success: function(collection,resp){
 			console.log('collection fetched', resp)
 			}}).done(function(){
@@ -26,6 +26,8 @@ App.module("Data", function(Data){
 				//create product collection, menu collection
 				Products = new Data.PageCollection(Data.Pages.where({description: 'gallery'}));
 				Menu = new Data.PageCollection(Data.Pages.where({description:'menu'}));
+				
+				WineOne = Products.models[0].get('children').at(0);
 			
 				//trigger callback because all data is ready
 				App.Data.ready.run();
